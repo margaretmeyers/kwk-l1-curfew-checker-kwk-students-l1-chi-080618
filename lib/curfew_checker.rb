b9ea12ea8cda1c2443eef77c26c98f42fa5aacef
margaretmeyers <marg.m.meyers@gmail.com>
2018-08-08 13:56:02 +0000

    automatically backed up by learn

--- lib/curfew_checker.rb
+++ lib/curfew_checker.rb
@@ -6,16 +6,16 @@ def simple_curfew_checker(time)
 
 end
 
-def curfew_checker(time)
-   if time == 11, 
-    puts "It's your curfew, go home!"
+# def curfew_checker(time)
+#   if time == 11, 
+#     puts "It's your curfew, go home!"
   
-  elsif time > 11 
-    puts "It's past your curfew, go home!"
+#   elsif time > 11 
+#     puts "It's past your curfew, go home!"
   
-  else
-    puts "It is not your curfew!"
-  end  
+#   else
+#     puts "It is not your curfew!"
+#   end  
 
 end
 
